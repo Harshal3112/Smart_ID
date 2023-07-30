b8502c6f57d77208690cf9452633c7ca6ad9f635
Harshal3112 <56355797+Harshal3112@users.noreply.github.com>
2023-07-30 21:22:12 +0530

    Add files via upload

---
+++ SpecialTopicProject(rfid)/CARD/card.d
@@ -0,0 +1,7 @@
+.\card.o: ..\..\PROJECTS\ST_PROJECT\CARD\CARD.c
+.\card.o: D:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.H
+.\card.o: D:\Keil\ARM\CMSIS\Include\core_cm3.h
+.\card.o: D:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\card.o: D:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\card.o: D:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\card.o: D:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
---
+++ SpecialTopicProject(rfid)/READER/rfid_reader.d
@@ -0,0 +1,7 @@
+.\rfid_reader.o: ..\..\RFID_READER.c
+.\rfid_reader.o: D:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.H
+.\rfid_reader.o: D:\Keil\ARM\CMSIS\Include\core_cm3.h
+.\rfid_reader.o: D:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\rfid_reader.o: D:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\rfid_reader.o: D:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\rfid_reader.o: D:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
